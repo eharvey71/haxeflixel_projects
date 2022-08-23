4ffbaff2a881d3137ede0cb9f64acf4cf123f66c
Eric Harvey <planetside0@gmail.com>
2022-08-23 19:52:55 -0400

    initial commit

---
+++ TurnBasedRPG/source/PlayState.hx
@@ -0,0 +1,17 @@
+package;
+
+import flixel.FlxState;
+import flixel.text.FlxText;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
